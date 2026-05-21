d747952b36c93a4d37c732cfbe59157aae652e7a
sloom2030 <nooralthbany@gmail.com>
2026-05-21 20:22:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from datetime import datetime, timezone
 from telegram import Bot
 from telegram.constants import ParseMode
 
-BOT_TOKEN = "8735462840:AAF5uJI6w5ZVUjxqy58rpawLJP4X_9v51A8"
-CHANCHANNEL_ID = "@AlphaSignalsBot"
+BOT_TOKEN = "8735462840:AAF5uJI6w5ZVUjxqy58rpawLJP4X_9v51A
+CHANNEL_ID = -1003924776124
 
 
 SCAN_INTERVAL_MINUTES = 15
